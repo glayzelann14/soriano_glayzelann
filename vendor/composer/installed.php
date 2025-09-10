8bdb7976a3d3ca7a5ea3a9916b7ba3b830739405
Glayzel Ann Soriano <glayzelannsoriano@gmail.com>
2025-09-10 16:12:46 +0800

    com

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9ecec7a099cf0e639c6f3dd12eba01467bddd3b1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9ecec7a099cf0e639c6f3dd12eba01467bddd3b1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
